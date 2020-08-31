1e406af9f6b7aadac58053469ff0b7c53c90a972
AlexeySadkovich <32710511+AlexeySadkovich@users.noreply.github.com>
2020-08-31 19:20:17 +0300

    Update views.py

--- src/taskmanager/views.py
+++ src/taskmanager/views.py
@@ -1,16 +1,1 @@
 from django.shortcuts import render
-from . import services
-
-
-def create_task(request):
-    """Processing task creating request"""
-    
-    services.add_task(services.parse_request(request))
-
-
-def delete_task(request):
-    pass
-
-
-def show_tasks(request):
-    pass
